389a552a29f05d77e9c3b3e05a809225820a86da
raviraj879214 <ravirajalphainfo@gmail.com>
2025-07-17 11:26:00 +0530

    upload

--- api/server.js
+++
@@ -1,9 +0,0 @@
-try {
-  const serverless = require('serverless-http');
-  const app = require('../src/app');
-
-  module.exports = serverless(app);
-} catch (err) {
-  console.error("Startup error:", err);
-  throw err;
-}
